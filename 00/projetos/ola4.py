4b1a887a553c0220af934e8a22108754c5592e17
profKalil <84177112+profKalil@users.noreply.github.com>
2025-10-15 21:04:15 -0300

    Update ola4.py

--- 00/projetos/ola4.py
+++ 00/projetos/ola4.py
@@ -1,12 +1,13 @@
 nome = input("Seu nome: ") 
-objeto = open("nomes.txt", "r")  
+objeto = open("nome.txt", "r")  
 while True: 	
 	if objeto.readline().strip() == nome:
 		print("Bem-vindo de volta", nome)
 		objeto.close()
 		break
 	else:
-		obj2 = open("nomes.txt", "a").write(nome + "\n")
+		obj2 = open("nome.txt", "a").write(nome + "\n")
 		print("Ola, ", nome)
 		obj2.close()
+
 		break
